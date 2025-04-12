572c4e14cece2d92b469f8b4f2b89a0e1d4134cf
ivan-hryshko <giver313@gmail.com>
2025-04-12 19:15:03 +0300

    feat: add APP_PORT check is exist

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,5 +1,8 @@
 
-const APP_PORT = process.env.APP_PORT
+const APP_PORT = process.env.APP_PORT || 8050
+if (!APP_PORT) {
+  console.error('APP_PORT is not defined in the environment variables')
+}
 const ENV_VARIABLES = {
   APP_PORT,
 }
