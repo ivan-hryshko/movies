5f8407158f8557a074ced3d1ff60269b406c20b7
ivan-hryshko <giver313@gmail.com>
2025-04-12 19:13:29 +0300

    feat: add APP_PORT and docker compose

---
+++ src/config/envs.ts
@@ -0,0 +1,7 @@
+
+const APP_PORT = process.env.APP_PORT
+const ENV_VARIABLES = {
+  APP_PORT,
+}
+
+export default ENV_VARIABLES
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import app from './app';
 import sequelize from './config/database';
-
-const port = 8050;
+import ENV_VARIABLES from './config/envs';
 
 const startServer = async () => {
   try {
@@ -11,8 +10,8 @@ const startServer = async () => {
     await sequelize.sync({ force: false });
     console.log('Models synchronized.');
 
-    app.listen(port, () => {
-      console.log(`Movies app listening at http://localhost:${port}`);
+    app.listen(ENV_VARIABLES.APP_PORT, () => {
+      console.log(`Movies app listening at http://localhost:${ENV_VARIABLES.APP_PORT}`);
     });
   } catch (error) {
     console.error('Unable to connect to the database:', error);
