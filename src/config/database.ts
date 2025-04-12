29d2ac85541e8ff9bd591ac7d3d90e26203e8c0e
ivan-hryshko <giver313@gmail.com>
2025-04-12 18:12:37 +0300

    feat: enable logging sequelize

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: './database.sqlite',
-  logging: false,
+  logging: true,
 });
 
 export default sequelize;
