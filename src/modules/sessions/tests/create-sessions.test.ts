bc8ce38b785d9498d274a4f9536d32853663cd09
ivan-hryshko <giver313@gmail.com>
2025-04-13 20:10:51 +0300

    test: fix create sessions

--- src/modules/sessions/tests/create-sessions.test.ts
+++ src/modules/sessions/tests/create-sessions.test.ts
@@ -3,24 +3,30 @@ import app from '../../../app';
 
 describe('POST /api/v1/sessions', () => {
   it('should create a session successfully', async () => {
-    const sessionData = {
+    const userData = {
       "email": "test@gmail.com",
       "name": "Ivan",
       "password": "12345678",
       "confirmPassword": "12345678"
     };
     const response = await request(app)
+      .post('/api/v1/users')
+      .send(userData);
+    expect(response.status).toBe(200);
+    const sessionData = {
+      "email": userData.email,
+      "password": userData.password
+    };
+    const sessionResponse = await request(app)
       .post('/api/v1/sessions')
       .send(sessionData);
-    expect(response.status).toBe(200);
-    expect(response.body.status).toBe(1);
-    expect(response.body.token).toBeDefined();
+    expect(sessionResponse.status).toBe(200);
+    expect(sessionResponse.body.status).toBe(1);
+    expect(sessionResponse.body.token).toBeDefined();
   });
   it('should not create a session without email', async () => {
     const sessionData = {
-      "name": "Ivan",
       "password": "12345678",
-      "confirmPassword": "12345678"
     };
     const response = await request(app)
       .post('/api/v1/sessions')
@@ -32,8 +38,6 @@ describe('POST /api/v1/sessions', () => {
   it('should not create a session without passwod', async () => {
     const sessionData = {
       "email": "test@gmail.com",
-      "name": "Ivan",
-      "confirmPassword": "12345678"
     };
     const response = await request(app)
       .post('/api/v1/sessions')
